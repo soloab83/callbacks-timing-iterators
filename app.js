913abbb26a258395f5817569b55de9a75f507698
Solomon Abraha <solomon@Solomons-MacBook-Pro.local>
2020-07-16 17:57:07 -0600

    [forEach] Added filters and map

--- app.js
+++ app.js
@@ -1,108 +1,144 @@
-function greeting(name, callback) {
-    let sentence = 'Hello, my name is ' + name + ' ';
+// function greeting(name, callback) {
+//     let sentence = 'Hello, my name is ' + name + ' ';
 
-    let otherSentence = callback();
+//     let otherSentence = callback();
 
-    return sentence + otherSentence;
-}
+//     return sentence + otherSentence;
+// }
 
-function anotherGreeting() {
-    let sentence = 'I forgot my last name';
+// function anotherGreeting() {
+//     let sentence = 'I forgot my last name';
 
-    return sentence;
-}
+//     return sentence;
+// }
 
-let myName = 'Solomon';
-console.log(greeting(myName, anotherGreeting));
+// let myName = 'Solomon';
+// console.log(greeting(myName, anotherGreeting));
 
 
-function printArray(array, callback) {
-    for (let i = 0; i < array.length; i++) {
-        let number = array[i];
+// function printArray(array, callback) {
+//     for (let i = 0; i < array.length; i++) {
+//         let number = array[i];
 
-        console.log(number);
-    }
+//         console.log(number);
+//     }
 
-    callback(array);
-}
+//     callback(array);
+// }
 
-function printArrayAgain(array) {
-    for (let i = 0; i < array.length; i++) {
-        let number = array[i];
+// function printArrayAgain(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         let number = array[i];
 
-        console.log(number);
-    }    
-}
+//         console.log(number);
+//     }    
+// }
 
-const numbersArray = [1,2,3,4,5,9,8,7,6];
+// const numbersArray = [1,2,3,4,5,9,8,7,6];
     
-printArray(numbersArray, printArrayAgain)
+// printArray(numbersArray, printArrayAgain)
 
 
-function calculator (num1, num2, callbackOne, callbackTwo) {
-    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+// function calculator (num1, num2, callbackOne, callbackTwo) {
+//     let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
 
-    return result;
-}
+//     return result;
+// }
 
-function addNumbers(num1, num2) {
-    let result = num1 + num2;
-
-
-}
-
-function subtractNumbers(num1, num2) {
-    let result = num1 - num2;
+// function addNumbers(num1, num2) {
+//     let result = num1 + num2;
 
 
-}
+// }
 
-console.log(calculator(5,77, addNumbers. subtractNumbers))
-
-
-// SuperHero Function
-
-function spiderMan (power1, power2,) {
-    let powers = power1() + ' and ' + power2();
-
-    return powers;
-}
-
-function spiderWeb() {
-    let power = 'I have spider web';
-
-    return power;
-}
-
-function spiderStrength() {
-    let strength = 'I have super strength';
-
-    return strength;
-}
-
-console.log(spiderMan(spiderWeb, spiderStrength));
+// function subtractNumbers(num1, num2) {
+//     let result = num1 - num2;
 
 
+// }
+
+// console.log(calculator(5,77, addNumbers. subtractNumbers))
 
 
-function blastOff() {
-    let num = 0;
+// // SuperHero Function
 
-    for (let i = 10; i >= num; i--) {
-        console.log(i);
+// function spiderMan (power1, power2,) {
+//     let powers = power1() + ' and ' + power2();
+
+//     return powers;
+// }
+
+// function spiderWeb() {
+//     let power = 'I have spider web';
+
+//     return power;
+// }
+
+// function spiderStrength() {
+//     let strength = 'I have super strength';
+
+//     return strength;
+// }
+
+// console.log(spiderMan(spiderWeb, spiderStrength));
+
+
+
+
+// function blastOff() {
+//     let num = 0;
+
+//     for (let i = 10; i >= num; i--) {
+//         console.log(i);
+//     }
+// }
+
+// setTimeout(blastOff, 5000);
+
+// function printName() {
+//     let num = 0;
+
+//     for (let i = 10; i >= num; i--) {
+//         console.log(i);
+//     } 
+
+//     console.log('SOLOMON!!!');
+// }
+
+// setTimeout(printName, 5000);
+
+
+//Iterators
+
+const collectables = ['Bomag', 'CMI Corp', 'Wirtgen', 'Caterpillar'];
+
+collectables.forEach(function (element) {
+    console.log(element)
+
+})
+
+collectables.forEach(element => {
+    if (element.length > 6) {
+        console.log(element);
     }
-}
+})
 
-setTimeout(blastOff, 5000);
+// map
 
-function printName() {
-    let num = 0;
+const SomeNumbers = [24, 65, 347, 34];
 
-    for (let i = 10; i >= num; i--) {
-        console.log(i);
-    } 
+const addFive = SomeNumbers.map(element => {
+    return element + 5;
+})
 
-    console.log('SOLOMON!!!');
-}
+console.log(addFive);
 
-setTimeout(printName, 5000);
+// filter
+const filteredNumber = SomeNumbers.filter(element => {
+    if (element > 50) {
+        return element;
+    }
+})
+
+console.log(filteredNumber);
+
