efac9f26beee99a6e7fe8f3ea36213ceddd65f78
Solomon Abraha <solomon@Solomons-MacBook-Pro.local>
2020-07-16 16:11:47 -0600

    [callback] Added greting and otherGreeting function

---
+++ app.js
@@ -0,0 +1,16 @@
+function greeting(name, callback) {
+    let sentence = 'Hello, my name is ' + name + ' ';
+
+    let otherSentence = callback();
+
+    return sentence + otherSentence;
+}
+
+function anotherGreeting() {
+    let sentence = 'I forgot my last name';
+
+    return sentence;
+}
+
+let myName = 'Solomon';
+console.log(greeting(myName, anotherGreeting));
